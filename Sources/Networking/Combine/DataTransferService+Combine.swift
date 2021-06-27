42cad34646c83c8ae3f6363f9d811507fa5c156c
Tim <sispo777@gmail.com>
2021-06-27 18:24:08 +0200

    DataTransferService + Combine extension

---
+++ Sources/Networking/Combine/DataTransferService+Combine.swift
@@ -0,0 +1,19 @@
+import Combine
+
+public extension DataTransferService where Self: AnyObject {
+    @available(iOS 13.0, *)
+    @discardableResult
+    func request<T: Decodable, E: ResponseRequestable>(
+        with endpoint: E
+    ) -> AnyPublisher<T, Error> where E.Response == T {
+        var task: NetworkCancellable?
+        
+        return Future() { [unowned self] promise in
+            task = request(with: endpoint, completion: { result in
+                promise(result)
+            })
+        }.handleEvents(receiveCancel: {
+            task?.cancel()
+        }).eraseToAnyPublisher()
+    }
+}
